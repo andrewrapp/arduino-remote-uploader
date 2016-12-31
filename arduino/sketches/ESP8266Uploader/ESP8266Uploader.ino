7485bda24806afe0777cad772a3d1450ec6d3c82
Andrew Rapp <andrew.rapp@gmail.com>
2016-12-31 07:38:46 -0700

    Added some additional notes and troubleshooting around esp8266 flakiness

--- arduino/sketches/ESP8266Uploader/ESP8266Uploader.ino
+++ arduino/sketches/ESP8266Uploader/ESP8266Uploader.ino
@@ -26,17 +26,23 @@
 #include <Esp8266.h>
 #include <SoftwareSerial.h>
 
-#define ESP_RX 3
-#define ESP_TX 4
+// NOTE: only some pins support softserial RX on Leonard and Mega boards
+#define ESP_RX 8
+#define ESP_TX 7
+
 #define ESP_PACKET_SIZE 32
 
 #define REPLY_SIZE 5
 #define RESET_PIN 9
 
+#define LED_PIN 13
+
+// TODO send ip address to server on boot for discovery. Otherwise we'd have to guess or query the router's dhcp table (not always possible)
+
 // FIXME client is getting socket timeouts. add ping method to restart if loses ip address
 // TODO proxy when not in programming mode
 
-// Tests the ESP communication
+// When true this only tests the ESP communication, does not use the remote uploader library.
 // This is useful to debug any issues with ESP using the serial port before throwing flashing into the mix
 #define COMMUNICATION_TEST false
 
@@ -52,47 +58,89 @@ uint8_t packet[ESP_PACKET_SIZE];
 RemoteUploader remoteUploader = RemoteUploader();
 extEEPROM eeprom = extEEPROM(kbits_256, 1, 64);
 
+/* 6/24 - at times getting ERROR or just an OK on listing networks (with FTDI). Also will not join a network. Could it be the access point rejecting?
+AT+CWLAP
+
+ERROR
+
+AT+CWLAP
+
+OK
+*/
+
+// 6/25 esp passes communication test on perfboard with DEBUG enabled, but once debug is set to false it gets an IPD error and sketch resets the radio
+// setup works fine. Could be the voltage divider can't handle the speed without debug slowdown
+// try leonardo with debug
+// try 3.3V Arduinos
+
 void setup() {
+  
+  pinMode(LED_PIN, OUTPUT);   
+    
   if (COMMUNICATION_TEST == false) {
     // for Leonardo use &Serial1
     // for atmega328/168 use &Serial
-    remoteUploader.setup(&Serial, &eeprom, RESET_PIN);    
-    //configure debug if an additional Serial port is available. Use Serial with Leonardo
+    if (remoteUploader.setup(&Serial, &eeprom, RESET_PIN) != SUCCESS) {
+      // eeprom fail
+      // optional error reporting
+      blink(2, 2000);
+    }
+    
+    //configure debug if an additional Serial port is available (e.g. Leonardo)
     //remoteUploader.setDebugSerial(&Serial);    
     
-    // NOTE: if you are using a 3.3V/8Mhz I highly recommend running Optiboot at 57.6K instead of 115.2K. This requires the 5V/16Mzh optiboot bootloader
-    remoteUploader.setBaudRate(OPTIBOOT_8MHZ_BAUD_RATE);
+    // NOTE: Only if you are using a 3.3V/8Mhz I highly recommend running Optiboot at 57.6K instead of 115.2K with 8Mhz Arduinos. This requires the 5V/16Mzh optiboot bootloader
+    //remoteUploader.setBaudRate(OPTIBOOT_8MHZ_BAUD_RATE);
   }
 
   // delay is recommended since if esp8266 is booting it will spew the boot garbage for about 3 seconds. any setup commands would fail
   delay(3000);  
+  
   espSerial.begin(9600);
   // configure esp to use softserial
   esp8266.setEspSerial(&espSerial);  
+  
   Serial.begin(9600); while(!Serial); // UART serial debug
   
-  // we can only use the Serial for debug when testing esp
   if (COMMUNICATION_TEST) {
-    esp8266.setDebugSerial(&Serial);    
+    // make sure esp library has #DEBUG enabled before uncommenting
+    //esp8266.setDebugSerial(&Serial);    
   }
   
-  // make sure this is big enough to read your packet or you'll get unexpected results
-  esp8266.setDataByteArray(packet, 32);
+  // set the packet array for receiving data
+  esp8266.setDataByteArray(packet, ESP_PACKET_SIZE);
 
-  // only the first time do you need to connect to the AP. this is retained by esp8266
+  // Perform first time esp configuration to connect to AP. Only need to run once
 //  if (esp8266.configure("ssid","password") != SUCCESS) {
 //    if (COMMUNICATION_TEST) {
 //      Serial.println("Configure failed");
 //    }
 //    
+//    blink(5, 1000);
 //    for (;;) {}
 //  }
 
-  while (!esp8266.configureServer(serverPort)) {
-      if (COMMUNICATION_TEST) {
-        Serial.println("Failed to configure server");
-      }
-      delay(3000);
+  while (!esp8266.configureServer(serverPort)) {      
+    blink(4, 1000);       
+    
+    if (COMMUNICATION_TEST) {
+      Serial.println("Failed to configure server");
+    }
+  }
+  
+  // indicate successful setup
+  blink(1, 50);  
+}
+
+void blink(int times, int wait) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(wait);
+    digitalWrite(LED_PIN, LOW);
+    
+    if (i < times - 1) {
+      delay(wait);      
+    }
   }
 }
 
@@ -154,6 +202,8 @@ void loop() {
         }
     } 
   } else if (esp8266.isError()) {
+    blink(3, 50);
+    
     if (COMMUNICATION_TEST) {
       Serial.print("Failed on command ");
       Serial.print(esp8266.getLastCommand());
@@ -168,7 +218,7 @@ void loop() {
         Serial.print("Resetting");
       }
       
-      // reset
+      // restart
       // assume the worst and reset
       esp8266.restartEsp8266();
       // need to apply server config which is lost on restart
