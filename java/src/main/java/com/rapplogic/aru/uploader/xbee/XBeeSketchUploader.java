4740f98de801b43b8793817476839f0b3abd1284
Andrew Rapp <andrew.rapp@gmail.com>
2016-12-31 20:12:56 -0700

    Support remote programming via xbee socket server. Added tx ack timeout to cmd args

--- java/src/main/java/com/rapplogic/aru/uploader/xbee/XBeeSketchUploader.java
+++ java/src/main/java/com/rapplogic/aru/uploader/xbee/XBeeSketchUploader.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.Map;
 
+import com.rapplogic.xbee.XBeeConnection;
+import com.rapplogic.xbee.socket.SocketXBeeConnection;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.OptionBuilder;
 
@@ -56,12 +58,13 @@ import com.rapplogic.xbee.api.zigbee.ZNetTxStatusResponse;
  */
 public class XBeeSketchUploader extends SketchUploader {
 
+	// TODO make this run as a server where it's connected to the radios. control via curl api
 	// TODO xbee just woke, send programming!
 	//final int WAKE = 4;
-	
 	// block size for eeprom writes
 	// TODO make this configurable. series 1 radios can support a larger payload
 	public final int XBEE_PAGE_SIZE = 64;
+	private int xbeeTxAckTimeoutMs = 500;
 
 	private final XBee xbee = new XBee();
 	
@@ -71,10 +74,10 @@ public class XBeeSketchUploader extends SketchUploader {
 		super();
 	}
 
-	public void flash(String file, String radioType, String device, int speed, String xbeeAddress, final boolean verbose, int ackTimeoutMillis, int arduinoTimeoutSec, int retriesPerPacket, int delayBetweenRetriesMillis) throws IOException {
+	public void flash(String file, String radioType, XBeeConnectionType xBeeConnectionType, String xbeeAddress, final boolean verbose, int ackTimeoutMillis, int arduinoTimeoutSec, int retriesPerPacket, int delayBetweenRetriesMillis) throws IOException {
 		Map<String,Object> context = Maps.newHashMap();
-		context.put("device", device);
-		context.put("speed", speed);
+		context.put("connectionType", xBeeConnectionType);
+
 		XBeeAddress64 xBeeAddress64 = null;
 		
 		try {
@@ -90,7 +93,14 @@ public class XBeeSketchUploader extends SketchUploader {
 
 	@Override
 	protected void open(final Map<String, Object> context) throws Exception {
-		xbee.open((String) context.get("device"), (Integer) context.get("speed"));
+
+		if (context.get("connectionType") instanceof LocalConnection) {
+			LocalConnection localConnection = (LocalConnection) context.get("connectionType");
+			xbee.open((String) localConnection.getSerialPort(), localConnection.getBaudRate());
+		} else {
+			SerialServerConnection ser = (SerialServerConnection) context.get("connectionType");
+			xbee.initProviderConnection((XBeeConnection)new SocketXBeeConnection(ser.getHost(), ser.getPort()));
+		}
 
 		final boolean verbose = (Boolean) context.get("verbose");
 		
@@ -144,7 +154,6 @@ public class XBeeSketchUploader extends SketchUploader {
 		});
 	}
 
-	final int TX_TIMEOUT = 500;
 	int counter = 1;
 	
 	@Override
@@ -152,19 +161,18 @@ public class XBeeSketchUploader extends SketchUploader {
 		Series series = (Series) context.get("series");
 		XBeeAddress64 address = (XBeeAddress64) context.get("xbeeAddress");
 
-		
 		try {
 			// TODO make configurable timeout
 			if (series == Series.SERIES1) {
-				xbee.sendSynchronous(new TxRequest64(address, data), TX_TIMEOUT);
+				xbee.sendSynchronous(new TxRequest64(address, data), xbeeTxAckTimeoutMs);
 			} else {
 				//xbee.sendAsynchronous(new ZNetTxRequest(address, data));
-				xbee.sendSynchronous(new ZNetTxRequest(address, data), TX_TIMEOUT);				
+				xbee.sendSynchronous(new ZNetTxRequest(address, data), xbeeTxAckTimeoutMs);
 			}
 		} catch (XBeeTimeoutException e) {
 			// TODO interrupt ack thread
 			// the ack will timeout so no need to do anything with this error
-			System.out.println("No tx ack after " + TX_TIMEOUT + "ms");
+			System.out.println("No tx ack from receiving XBee after " + xbeeTxAckTimeoutMs + "ms");
 		}
 	}
 
@@ -181,7 +189,10 @@ public class XBeeSketchUploader extends SketchUploader {
 	public final static String radioType = "radio-type";
 	public final static String serialPort = "serial-port";
 	public final static String baudRate = "baud-rate";
+	public final static String serialServerHost = "serial-server-host";
+	public final static String serialServerPort = "serial-server-port";
 	public final String xbeeAddress = "remote-xbee-address";
+	public final String xbeeTxAckTimeout = "xbee-tx-ack-timeout";
 	
 	private void runFromCmdLine(String[] args) throws org.apache.commons.cli.ParseException, IOException {
 		CliOptions cliOptions = getCliOptions();
@@ -196,29 +207,55 @@ public class XBeeSketchUploader extends SketchUploader {
 		
 		cliOptions.addOption(
 				OptionBuilder
-				.withLongOpt(serialPort)
-				.hasArg()
-				.isRequired(true)
-				.withDescription("Serial port of of local xbee (host) radio) (e.g. /dev/tty.usbserial-A6005uRz). Required")
-				.create("p"));
-
-		cliOptions.addOption(
-				OptionBuilder
-				.withLongOpt(baudRate)
-				.hasArg()
-				.isRequired(true)
-				.withType(Number.class)
-				.withDescription("Baud rate of local xbee (host) radio serial port. Required")
-				.create("b"));
-		
-		cliOptions.addOption(
-				OptionBuilder
 				.withLongOpt(xbeeAddress)
 				.hasArg()
 				.isRequired(true)
 				.withDescription("Address (64-bit) of remote XBee radio (e.g. 0013A21240AB9856)")
 				.create("x"));
-		
+
+		cliOptions.addOption(
+				OptionBuilder
+						.withLongOpt(serialPort)
+						.hasArg()
+						.isRequired(false)
+						.withDescription("Serial port of of local xbee (host) radio) (e.g. /dev/tty.usbserial-A6005uRz)")
+						.create("p"));
+
+		cliOptions.addOption(
+				OptionBuilder
+						.withLongOpt(baudRate)
+						.hasArg()
+						.isRequired(false)
+						.withType(Number.class)
+						.withDescription("Baud rate of local xbee (host) radio serial port")
+						.create("b"));
+
+		cliOptions.addOption(
+				OptionBuilder
+						.withLongOpt(serialServerHost)
+						.hasArg()
+						.isRequired(false)
+						.withDescription("Host or ip address of xbee serial server")
+						.create("j"));
+
+		cliOptions.addOption(
+				OptionBuilder
+						.withLongOpt(serialServerPort)
+						.hasArg()
+						.isRequired(false)
+						.withType(Number.class)
+						.withDescription("Listen port of xbee serial server")
+						.create("k"));
+
+		cliOptions.addOption(
+				OptionBuilder
+						.withLongOpt(xbeeTxAckTimeout)
+						.hasArg()
+						.isRequired(false)
+						.withType(Number.class)
+						.withDescription("How long to wait to receive and an ACK from the receiving XBee in ms. Although flashing can succeed if these timeous occur, it's best to wait for an ACK before sending the next comand. Bump this up when using a serial server. Default is " + xbeeTxAckTimeoutMs + " ms")
+						.create("l"));
+
 		cliOptions.build();
 		
 		CommandLine commandLine = cliOptions.parse(args);
@@ -232,14 +269,31 @@ public class XBeeSketchUploader extends SketchUploader {
 				System.err.println(radioType + " must be series1 or series2");
 				return;
 			}
-					
+
+			if (commandLine.getOptionValue(xbeeTxAckTimeout) != null) {
+				xbeeTxAckTimeoutMs = cliOptions.getIntegerOption(xbeeTxAckTimeout);
+			}
+
+			XBeeConnectionType xBeeConnectionType = null;
+
+			if ((commandLine.getOptionValue(serialPort) == null || commandLine.getOptionValue(baudRate) == null) &&
+					(commandLine.getOptionValue(serialServerHost) == null || commandLine.getOptionValue(serialServerPort) == null)) {
+				System.err.println("Either a serial port and baudrate must be specified or a serial server host/port");
+				return;
+			} else {
+				if (commandLine.getOptionValue(serialPort) != null) {
+					xBeeConnectionType = new LocalConnection(commandLine.getOptionValue(serialPort), cliOptions.getIntegerOption(baudRate));
+				} else {
+					xBeeConnectionType = new SerialServerConnection(commandLine.getOptionValue(serialServerHost), cliOptions.getIntegerOption(serialServerPort));
+				}
+			}
+
 			// cmd line
-			new XBeeSketchUploader().flash(
+			flash(
 					commandLine.getOptionValue(CliOptions.sketch),
 					commandLine.getOptionValue(radioType),
-					commandLine.getOptionValue(serialPort), 
-					cliOptions.getIntegerOption(baudRate), 
-					commandLine.getOptionValue(xbeeAddress), 
+					xBeeConnectionType,
+					commandLine.getOptionValue(xbeeAddress),
 					commandLine.hasOption(CliOptions.verboseArg),
 					cliOptions.getIntegerOption(CliOptions.ackTimeoutMillisArg),
 					cliOptions.getIntegerOption(CliOptions.arduinoTimeoutArg),
@@ -284,5 +338,45 @@ ceylon:arduino-remote-uploader-1.0-SNAPSHOT andrew$
 		XBeeSketchUploader xBeeSketchUploader = new XBeeSketchUploader();
 		xBeeSketchUploader.runFromCmdLine(args);
 		//new XBeeSketchUploader().processXBee("/Users/andrew/Documents/dev/arduino-remote-uploader/resources/BlinkSlow.cpp.hex", "/dev/tty.usbserial-A6005uRz", Integer.parseInt("9600"), "0013A200408B98FF", false, 5, 0, 500, 0);
+	}
+
+	static class XBeeConnectionType {
+
+	}
+
+	static class SerialServerConnection extends XBeeConnectionType {
+		private String host;
+		private int port;
+
+		public SerialServerConnection(String host, int port) {
+			this.host = host;
+			this.port = port;
+		}
+
+		public String getHost() {
+			return host;
+		}
+
+		public int getPort() {
+			return port;
+		}
+	}
+
+	static class LocalConnection extends XBeeConnectionType {
+		private String serialPort;
+		private int baudRate;
+
+		public String getSerialPort() {
+			return serialPort;
+		}
+
+		public int getBaudRate() {
+			return baudRate;
+		}
+
+		public LocalConnection(String serialPort, int baudRate) {
+			this.serialPort = serialPort;
+			this.baudRate = baudRate;
+		}
 	}
 }
